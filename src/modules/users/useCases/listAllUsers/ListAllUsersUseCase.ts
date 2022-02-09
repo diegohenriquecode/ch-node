eb6451a4fe1dc73b03cff776f230aa929f7352ac
Diego | Tradex <diego.oliveira@tradexsolutions.com.br>
2022-02-09 06:48:33 -0300

    finalized

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,22 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if (!userIsAdmin.admin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
